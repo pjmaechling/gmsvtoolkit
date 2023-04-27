631d578ec3b99be8ef49e2dcbf0e9dc5e32cc767
Philip Maechling <pjmaechling@users.noreply.github.com>
2023-04-26 17:42:40 -0700

    updated

--- dockerbuild/run_gtk.sh
+++ dockerbuild/run_gtk.sh
@@ -1,3 +1,3 @@
 # -rm removes container on exit
 # -it indicates inteactive + --tty so that users are at an interactive tty command line when the container starts
-docker run -p 8888:8888 --rm -i -t --mount type=bind,source="$(pwd)"/target,destination=/home/gtkuser/target gtk_jup:latest
+docker run -p 8888:8888 --rm -i -t --mount type=bind,source="$(pwd)"/target,destination=/home/gtkuser/target sceccode/gtk_jup:latest
